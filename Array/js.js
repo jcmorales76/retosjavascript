843a74b7c7dcea03ce432d5c0d73970c2920ac15
Juan Carlos Morales R <43224641+jcmorales76@users.noreply.github.com>
2022-09-10 09:36:57 -0500

    Desafio Funciones de orden superior

---
+++ Array/js.js
@@ -0,0 +1,64 @@
+const empleado = parseInt(prompt("¿Cuántos empleados deseas Registrar? "));
+
+class persona {
+  constructor({ nombre, apellido, correo, tipoUsuario, horasv, licencia }) {
+    (this.nombre = nombre),
+      (this.apellido = apellido),
+      (this.correo = correo),
+      (this.tipoUsuario = tipoUsuario);
+      (this.horasv = horasv);
+      (this.licencia = licencia);
+  }
+  
+}
+
+const registro = [];
+for (let index = 0; index < empleado; index++) {
+  const nombre = prompt("Ingresa nombre de empleado: ");
+  const apellido = prompt("Ingrese su apellido: ");
+  const correo = prompt("Ingrese tu correo: ");
+  const tipoUsuario = parseInt(prompt( `Elige un tipo de Usuario: "1. Piloto" - "2. Mecanico"`));
+
+
+    if (tipoUsuario === 1){
+        alert(`Registro ingresado de ${nombre} ${apellido} ${correo} Usted es PILOTO`)
+        
+    }else if(tipoUsuario === 2){
+        alert(`Registro ingresado de ${nombre} ${apellido} ${correo} Usted es MECANICO`) 
+        
+    }else{
+        alert("Usted ingreso dato incorrecto")
+    }; 
+
+
+
+  registro.push(new persona({ nombre, apellido, correo, tipoUsuario }));
+ 
+}
+
+console.log(registro);
+
+alert(`Buscaremos empleados`);
+
+let caso = prompt("Condicion de busqueda: 1-Por Nombres y 2-Por Apellidos ")
+
+switch (caso) {
+    case "1":
+        let nombre = prompt("Buscar - Ingrese nombre del la persona");
+        let busqueda = registro.find(item => item.nombre === nombre);
+        console.log(busqueda);
+        break;
+    case "2":
+        let apellido = prompt("Buscar - Ingrese Apellido del la persona");
+        let busqueda2 = registro.find(item => item.apellido === apellido);
+        console.log(busqueda2);
+        break;
+    default:
+        console.log(`Ingrese una opcion correcta`)
+}
+
+
+
+
+
+
