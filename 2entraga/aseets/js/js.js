9b40b8af01311d0d5c209cb55f0ad37fb797883b
jcmorales <jcmorales51@gmail.com>
2022-09-28 18:46:53 -0500

    2da entrega

--- 2entraga/aseets/js/js.js
+++ 2entraga/aseets/js/js.js
@@ -10,8 +10,25 @@ const personas = [
   { id: 8, nombre: "Carlos", apellido: "Casasola", tipoUsuario: "Mecanico" },
 ];
 
+localStorage.setItem("personas", JSON.stringify(personas));
+
+const agregarStorage = (nombre, apellido, tipoUsuario) => {
+  let personas = JSON.parse(localStorage.getItem("personas"));
+  let persona = {
+    id: personas.length + 1,
+    nombre: nombre,
+    apellido: apellido,
+    tipoUsuario: tipoUsuario,
+  };
+
+  personas.push(persona);
+  localStorage.setItem("personas", JSON.stringify(personas));
+};
+
 let contenedor = document.getElementById("contenedor");
 let btnRegistro = document.getElementById("btnRegistro");
+let btnBuscar = document.getElementById("btnBuscar");
+let btnModificar = document.getElementById("btnModificar");
 let btnBorrar = document.getElementById("btnBorrar");
 
 //Ingreso de Datos
@@ -20,6 +37,8 @@ const ingresoDatos = () => {
   let nombre = prompt("Ingrese el nombre del empleado");
   let apellido = prompt("Ingrese el apellido del empleado");
   let tipoUsuario = prompt("Ingrese Piloto o Mecanico");
+
+  agregarStorage(nombre, apellido, tipoUsuario);
 
   let registroPersonas = {
     id: personas.length + 1,
@@ -42,6 +61,25 @@ const ingresoDatos = () => {
   alert("Registro ingresado");
 };
 
+/*
+const cambiarTipoUsuario = (nombre, _apellido, tipoUsuario) => {
+  contenedor.innerHTML = "";
+
+
+  let personas = JSON.parse(localStorage.getItem("personas"));
+
+  personas.find(item => item.nombre === nombre).tipoUsuario = tipoUsuario;
+
+  // personas.find((item) => item.nombre === nombre);
+  // persona.tipoUsuario = tipoUsuario;
+
+  localStorage.setItem("personas", JSON.stringify(personas));
+};
+
+cambiarTipoUsuario(nombre, apellido, tipoUsuario);
+
+*/
+
 //Borrado de Pantala
 const borrarPantalla = () => {
   contenedor.innerHTML = "";
@@ -50,13 +88,15 @@ const borrarPantalla = () => {
 //buscar datos
 const buscarDatos = () => {
   let buscar = prompt("Ingrese nombre a buscar");
-  let encontrado = personas.find((item) => item.personas === buscar);
+  personas.find((item) => item.personas === buscar);
 
-  let div = document.createElement("div");
-  div.innerHTML = `<h4>Empleado : ${id}</h4>
+  localStorage.setItem("personas", JSON.stringify(personas));
+
+  let h3 = document.createElement("div");
+  h3.innerHTML = `<h4>Empleado : ${id}</h4>
                     <h3>Nombre : ${nombre} ${apellido}</h3>
                     <h3> Empleado : ${tipoUsuario}</h3>
-                    ------------------------------------------`;
+                    ------------------------------------------`; 
   contenedor.append(div);
 };
 
@@ -64,4 +104,5 @@ const buscarDatos = () => {
 let evento = "click";
 btnRegistro.addEventListener(evento, ingresoDatos);
 btnBorrar.addEventListener(evento, borrarPantalla);
+//btnModificar.addEventListener(evento, cambiarTipoUsuario);
 btnBuscar.addEventListener(evento, buscarDatos);
